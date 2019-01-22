e298e45ab9f71b3884e8b5eed8cb8cdc86869888
Ashton <ashtonenglish3@gmail.com>
2019-01-22 16:34:08 +0200

    learnt the basics of frameworks

---
+++ js/backend.js
@@ -0,0 +1,20 @@
+// requeste data from a backened
+
+// Put simply;HTTP is a protocol which allows us to send info around the web.For example,when your browser asks for http://domain.HTTP requests come in differnet flavors,using different verbs(GET,POST,PUT, and DELETE are the four most common.)THe kind of request your browser makes(and the kind we will use in this chapter) is a GET request.A GET request generally mans we would like the sever to send us some data.
+
+
+// Dont worry too much about the details for now once youve seen HTTP requests in practice,it becomes a lot easier to understand.
+
+// AJAX stands for Asynchronous Jaavaascript and XML.Now theres a mouthful.
+
+
+// Basically it is a systen that allow us to send requested and handle the responses from withiin our javascript code.For example,whenyou leve a comment on a blog post,when you send or receive e-mail on a webmail service like GMail,or when a webpage updates itself without refreshing ,it is often using AJAX to do so.
+
+
+// Two other initialisms you may see in this context are API and JSON.
+
+//    API stands for Application Progrmming Interface.MAny companies and services proc=vide APIs which will react in specific ways when we send them HTTP requested.You will come accross a few in this.
+
+
+
+// JSON or JAvascript OBject notation, is a way of representing and handling dataa as a Javascript object .
